d9e048219a0d8dcaadc0e4ced7b4bb0fc16a233c
Chad Clark <chadclark12@hotmail.com>
2021-02-02 23:46:30 -0600

    home page

--- Develop/server.js
+++ Develop/server.js
@@ -2,21 +2,24 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const importData = require("./db/db.json");
-let port = 3001;
+let PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // SENDS USER TO FIRST AJAX PAGE \\
 app.get("/", (req, res) => {
-  res.send("Welcome to the Note Taker Page!");
-  // res.sendFile(path.join(__dirname, "view.html"));
+  // res.send("Welcome to the Note Taker Page!");
+  res.sendFile(__dirname + "public/index.html");
 });
 
-// DB.JSON PAGE \\
+// DISPLAY NOTE PAGE WHEN CLICK \\
 app.get("/notes", (req, res) => {
-  res.send(importData);
-  // res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(__dirname + "/public/notes.html");
 });
 
 // STARTS SERVER AND BEGINS LISTENING \\
-app.listen(port, function () {
-  console.log(`App listening on port http://localhost:${port}`);
+app.listen(PORT, function () {
+  console.log(`App listening on port http://localhost:${PORT}`);
 });
