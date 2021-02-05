d5ccb8f569c9745a73304a9d2ab409cc0b5205ad
Chad Clark <chadclark12@hotmail.com>
2021-02-04 18:09:41 -0600

    add id on note

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,29 +1,30 @@
 const path = require("path");
 const fs = require("fs");
-// const uuid = require("uuid");
+const uuid = require("uuid");
 const noteData = require("../db/db.json");
 
 module.exports = (app) => {
-  //CREATE NOTE AND ADD ID \\
+  // GRAB NOTES AND DISPLAY ON PAGE \\
   app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../db/db.json"));
-    console.log();
+    console.log("hello");
     // res.json(tableData);
   });
 
-  // RETRIEVE NOTE BY ID \\
+  // RECEIVE NEW NOTE/ADD ID/ADD TO ARRAY \\
   app.post("/api/notes", (req, res) => {
     const newNote = req.body;
+    newNote.id = uuid.v4();
     const recallArr = JSON.parse(fs.readFileSync("db/db.json"));
     recallArr.push(newNote);
     console.log(newNote);
     console.log(recallArr);
     fs.writeFileSync("db/db.json", JSON.stringify(recallArr));
-    // res.json(waitListData);
+    res.json(newNote);
   });
 
   // DELETE NOTE BY ID \\
-  app.delete("/api/notes", (req, res) => {
+  app.delete("/api/notes:id", (req, res) => {
     console.log("delete note");
     // if (tableData.length < 5) {
     //   tableData.push(req.body);
